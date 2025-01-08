bb1f536949fd34ecf716ab8e92c0dc6da3e47c3e
tommyme-wsl <tommyme@wsl.com>
2025-01-08 22:14:51 +0800

    new tmux support

--- main/tmux/install.sh
+++ main/tmux/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-ln -s -f "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
-cp "$SCRIPT_DIR/.tmux.conf.local" ~/.tmux.conf.local
+mkdir -p ~/.config/tmux/ ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -sf "$SCRIPT_DIR/tmux.conf" ~/.config/tmux/tmux.conf
+ln -sf "$SCRIPT_DIR/tmux.reset.conf" ~/.config/tmux/tmux.reset.conf
+echo "if it's first time to start tmux, press <ctrl-a I> to install dependencies."
